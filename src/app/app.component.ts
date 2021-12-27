6c0c506b0d0c35d976233a7433fac2ed4eeab885
DiegoSantos90 <dr.santos1990@gmail.com>
2021-12-27 17:43:46 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,11 +14,24 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     of(2, 4, 6, 8).subscribe(console.log);
 
-    from([20, 15, 10, 5]).subscribe({
-      next: (item) => console.log(`resulting item ...${item}`),
-      error: (err) => console.error(`error occurred ${err}`),
-      complete: () => console.log(`complete!!!`),
-    });
+    from([20, 15, 10, 5])
+      .pipe(
+        tap((item) => console.log(`emitted item ... ${item}`)),
+        map((item) => item * 2),
+        map((item) => item - 10),
+        map((item) => {
+          if (item === 0) {
+            throw new Error('zero detected');
+          }
+          return item;
+        }),
+        take(3)
+      )
+      .subscribe({
+        next: (item) => console.log(`resulting item ...${item}`),
+        error: (err) => console.error(`error occurred ${err}`),
+        complete: () => console.log(`complete!!!`),
+      });
 
     of('Apple', 'Samsung', 'Xiaomi').subscribe({
       next: (item) => console.log(`I buy an ${item}`),
